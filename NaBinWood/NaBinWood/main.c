c4cd4d8079132f25f69d73ae63e826a929c63313
나 <nalaj280@gmail.com>
2026-05-18 17:14:40 +0900

    통합_1

---
+++ NaBinWood/NaBinWood/main.c
@@ -0,0 +1,116 @@
+﻿#include <stdio.h>
+#include <wchar.h>
+#include <locale.h>
+#include <windows.h>
+#include <fcntl.h>
+#include <io.h>
+
+#pragma warning(disable:4996)
+
+HANDLE hConsole;
+
+void setColor(int color)
+{
+    SetConsoleTextAttribute(hConsole, color);
+}
+
+void printChar(wchar_t ch)
+{
+    DWORD written;
+
+    WriteConsoleW(
+        hConsole,
+        &ch,
+        1,
+        &written,
+        NULL
+    );
+}
+
+int main()
+{
+    char path[MAX_PATH];
+
+    GetCurrentDirectoryA(MAX_PATH, path);
+
+    MessageBoxA(NULL, path, "현재 경로", MB_OK);
+
+    // 콘솔 핸들
+    hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    // UTF-16 출력 모드
+    _setmode(_fileno(stdout), _O_U16TEXT);
+
+    // 콘솔 설정
+    system("mode con cols=170 lines=60");
+    system("color 00");
+
+    // UTF-8 텍스트 파일 읽기
+    FILE* fp = fopen("art.txt", "rb");
+
+
+
+    if (!fp)
+    {
+        wprintf(L"art.txt 파일을 열 수 없습니다.\n");
+        system("pause");
+        return 1;
+    }
+
+    // UTF-8 → UTF-16 변환용 버퍼
+    char utf8Buffer[4096];
+    wchar_t wideBuffer[4096];
+
+    int line = 0;
+
+    while (fgets(utf8Buffer, sizeof(utf8Buffer), fp))
+    {
+        // UTF-8 -> UTF-16 변환
+        MultiByteToWideChar(
+            CP_UTF8,
+            0,
+            utf8Buffer,
+            -1,
+            wideBuffer,
+            4096
+        );
+
+        int x = 0;
+        wchar_t* p = wideBuffer;
+
+        while (*p)
+        {
+            wchar_t ch = *p;
+
+            // 기본 흰색
+            setColor(7);
+
+            // 특정 영역 노랑색
+            if (
+                line >= 14 && line <= 23 &&
+                x >= 56 && x <= 121 &&
+                ch != L' '
+                )
+            {
+                setColor(6);
+            }
+
+            printChar(ch);
+
+            p++;
+            x++;
+        }
+
+        line++;
+    }
+
+    fclose(fp);
+
+    setColor(7);
+
+    wprintf(L"\n\n");
+
+    system("pause");
+
+    return 0;
+}
---
+++ NaBinWood/escapeGAME/main.c
@@ -0,0 +1,148 @@
+#include <stdio.h>
+
+#define COLOR_RESET "\x1b[0m"
+
+#define FONT_COLOR_BLACK 30
+#define BG_COLOR_BLACK 40
+#define FONT_COLOR_RED 31
+#define BG_COLOR_RED 41
+#define FONT_COLOR_GREEN 32
+#define BG_COLOR_GREEN 42
+#define FONT_COLOR_YELLOW 33
+#define BG_COLOR_YELLOW 43
+#define FONT_COLOR_BLUE 34
+#define BG_COLOR_BLUE 44
+#define FONT_COLOR_MAGENTA 35
+#define BG_COLOR_MAGENTA 45
+#define FONT_COLOR_BRIGHTMAGENTA 95
+#define BG_COLOR_BRIGHTMAGENTA 105
+#define FONT_COLOR_WHITE 37
+#define BG_COLOR_WHITE 47
+
+
+void set_color(int code);
+int move_cursor(int x, int y);
+
+int menu = 1;
+int isRunning = 1;
+
+int RenderTitle()
+{
+	set_color(BG_COLOR_BRIGHTMAGENTA);
+	set_color(FONT_COLOR_WHITE);
+	move_cursor(50, 9);
+	printf("                      ");
+	move_cursor(50, 10);
+	printf(" 이은석:추격의 시작  ");
+	move_cursor(50, 11);
+	printf("                      ");
+
+	set_color(BG_COLOR_BLACK);
+
+	if (menu == 1)
+	{
+		set_color(BG_COLOR_YELLOW);
+	}
+	move_cursor(52, 13);
+	printf("  1. 시작  ");
+	set_color(BG_COLOR_BLACK);
+
+	if (menu == 2)
+	{
+		set_color(BG_COLOR_YELLOW);
+	}
+	move_cursor(52, 15);
+	printf("  2. 게임설명  ");
+	set_color(BG_COLOR_BLACK);
+
+	if (menu == 3)
+	{
+		set_color(BG_COLOR_YELLOW);
+	}
+	move_cursor(52, 17);
+	printf("  3. 팀소개  ");
+	set_color(BG_COLOR_BLACK);
+
+	if (menu == 4)
+	{
+		set_color(BG_COLOR_YELLOW);
+	}
+	move_cursor(52, 19);
+	printf("  4. 게임종료  ");
+	set_color(BG_COLOR_BLACK);
+
+	char a = getch();
+	switch (a)
+	{
+	case 'w':
+		if (menu > 1)
+		{
+			menu = menu - 1;
+		}
+		break;
+	case 's':
+		if (menu < 4)
+		{
+			menu = menu + 1;
+		}
+		break;
+	case 13:
+		if (menu == 1) {
+			system("cls");
+			menu = 0;
+			return 2;
+
+			break;
+		}
+		else if (menu == 4)
+		{
+			isRunning = 0;
+
+		}
+	}
+
+	return 0;
+}
+
+int MainGame()
+{
+	printf("game");
+}
+
+void set_color(int code)
+{
+	printf("\x1b[%dm", code);
+}
+
+int move_cursor(int x, int y)
+{
+	printf("\033[%d;%dH", y, x);
+	return 0;
+}
+
+int main()
+{
+	system("chcp 65001");
+	system("cls");
+
+	int gameStatus = 0;
+
+	while (isRunning)
+	{
+		switch (gameStatus)
+		{
+		case 0:
+			gameStatus = RenderTitle();
+			break;
+		case 2:
+			gameStatus = MainGame();
+			break;
+		}
+
+	}
+
+	system("cls");
+	move_cursor(0, 25);
+
+	return 0;
+}
